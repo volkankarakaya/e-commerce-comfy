2b6f168d97fae69f03abd38a700b043128064c3a
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-06-15 14:42:42 +0300

    add single product page structure

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -51,7 +51,8 @@ const products_reducer = (state, action) => {
   if (action.type === GET_SINGLE_PRODUCT_ERROR) {
     return {
       ...state,
-      single_product_loading: true,
+      single_product_loading: false,
+      single_product_error: true,
     };
   }
 
